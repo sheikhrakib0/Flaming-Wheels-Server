e81fa38cb90653da3e5650c44cfeef92d0913887
sheikhrakib0 <rakibrakib0179@gmail.com>
2021-11-13 18:20:21 +0600

    requirement

--- index.js
+++ index.js
@@ -23,18 +23,21 @@ async function run() {
 
     const database = client.db("FlamingWheels");
     const productsCollection = database.collection("products");
+    const reviewCollection = database.collection("reviews");
+    const orderCollection = database.collection("orders");
+    const usersCollection = database.collection("users");
 
     app.get('/', (req, res) => {
       res.send("I am from server side for mongo & crud");
     });
     //posting product
     app.post('/products', async (req, res) => {
-      const doc = {
-        name: 'cycle',
-        price: 1200
-      }
-      const result = await productsCollection.insertOne(doc);
-      console.log(`A document was inserted with the _id: ${result.insertedId}`);
+      // const doc = {
+      //   name: 'cycle',
+      //   price: 1200
+      // }
+      const result = await productsCollection.insertOne();
+      // console.log(`A document was inserted with the _id: ${result.insertedId}`);
       res.send(result);
     })
 
@@ -48,12 +51,74 @@ async function run() {
     //getting single product
     app.get('/products/:id', async (req, res) => {
       const id = req.params.id;
-      console.log(id)
       const query = { _id: ObjectId(id) };
       const product = await productsCollection.findOne(query);
-      console.log('loading user data with id', id);
+      // console.log('loading user data with id', id);
 
       res.send(product);
+    })
+
+    // getting all reviews
+    app.get('/reviews', async(req, res)=>{
+      const cursor = reviewCollection.find({});
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    })
+    //posting review
+    app.post('/reviews', async(req, res)=>{
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.json(result);
+    });
+
+    //posting orders
+    app.post('/orders', async(req, res)=>{
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.json(result);
+    });
+    //getting orders
+    app.get('/orders', async(req, res)=>{
+      const email = req.query.email;
+      const query = {email: email};
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
+    })
+
+    // creating users
+    app.post('/users', async(req, res)=>{
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.json(result);
+    })
+    //getting a sigle user to chack admin status
+    app.get('/users/:email', async(req, res)=>{
+      const email = req.params.email;
+      const query = {email: email };
+      let isAdmin = false;
+      const user = await usersCollection.findOne(query);
+      if(user?.role === 'admin'){
+        isAdmin = true;
+      }
+      res.json({admin: isAdmin});
+    })
+    //update users
+    app.put('/users', async(req,res)=>{
+      const user = req.body;
+      const filter = {email: user.email };
+      const options = { upsert: true};
+      const updateDoc = {$set: user};
+      const result = await usersCollection.updateOne(filter, updateDoc, options);
+      res.json(result);
+    })
+    //making admin by adding role to users
+    app.put('/users/admin', async(req, res)=>{
+      const user = req.body;
+      const filter = {email: user.email};
+      const updateDoc = {$set: {role: 'admin'}};
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.json(result);
     })
   }
   finally {
